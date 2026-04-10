988d963ede87ba1efc45dab17820a827ebcd9704
Sheila <smercado2468@gmail.com>
2026-04-09 23:31:28 -0400

    finished character movements
    Character now moves from left to right by flipping the side frame instead of individual frames.

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,10 +1,24 @@
 extends CharacterBody2D
 
 @export var speed = 400
+@onready var animated_sprite = $AnimatedSprite2D
+
+var last_dir = "down"
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
+	
+	if velocity.length() > 0:
+		#updates flip
+		if velocity.x != 0:
+			animated_sprite.flip_h = velocity.x < 0
+			last_dir = "side"
+		elif velocity.y != 0:
+			last_dir = "down" if velocity.y > 0 else "up"
+		animated_sprite.play("walking " + last_dir)
+	else:
+		animated_sprite.play("idle " + last_dir)
 
 func _physics_process(delta):
 	get_input()
